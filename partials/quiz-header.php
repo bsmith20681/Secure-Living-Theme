cedd7e949bd6deaa92cfe424f1ada5a5bbdd4d35
Ben <bsmith20681@gmail.com>
2026-03-06 09:53:33 -0500

    updated phone numbers

--- partials/quiz-header.php
+++ partials/quiz-header.php
@@ -75,7 +75,7 @@
                             </div>
                         </div>
                         <div class="call-specialist__list">
-                            <a href="tel:18332247221" class="call-specialist__company">
+                            <a href="tel:18333227030" class="call-specialist__company">
                                 <div class="call-specialist__logo">
                                     <img src="http://secureliving.com/wp-content/uploads/2026/02/adt-logo-sm.png" alt="adt logo">
                                 </div>
@@ -87,10 +87,10 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
                                     </svg>
-                                    (833) 224-7221
+                                    (833) 322-7030
                                 </div>
                             </a>
-                            <a href="tel:18336062573" class="call-specialist__company">
+                            <a href="tel:18334367355" class="call-specialist__company">
                                 <div class="call-specialist__logo">
                                     <img src="http://secureliving.com/wp-content/uploads/2026/02/vivint-logo-sm.png" alt="vivint logo">
                                 </div>
@@ -102,7 +102,7 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
                                     </svg>
-                                    (833) 606-2573
+                                    (833) 436-7355
                                 </div>
                             </a>
                         </div>
